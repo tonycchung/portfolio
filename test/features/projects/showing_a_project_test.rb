da9a23eb0b72e377d15ff142fc24da386d29f143
Tony Chung <anthonyuchung@gmail.com>
2014-05-12 21:49:44 -0700

    Adding tests for showing a project and the project index page. Passing both tests.

---
+++ test/features/projects/showing_a_project_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+feature "As a user I want to see individual projects" do
+  scenario "viewing a single project" do
+    # Given some posted projects
+    visit projects_path
+    # When I click on a single project
+    first(:link, "Show").click
+    # Then I can view the individual project
+    page.text.must_include "My favorite sports"
+    page.text.wont_include "This is a fixture title"
+  end
+end
